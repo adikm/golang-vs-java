928d2683aa630053b42d31592444098a241eba94
Bruno Borges <bruno.borges@gmail.com>
2020-02-05 14:09:22 -0800

    Update optional_init.java

--- content/structures/optional_init.java
+++ content/structures/optional_init.java
@@ -1,14 +1,6 @@
-public class Student {
-    private String name;
-    private int age;
-
+public record Student(String name, int age) {
     public Student(String name) {
-        this.name = name;
-    }
-
-    public Student(String name, String age) {
-        this.name = name;
-        this.age = age;
+        this(name, 0);
     }
 }
 
