5e10f1e47c48ca5364723cff9c21c9c6ddcb0c11
Adrian Marszałek <contact@amarszalek.net>
2020-02-05 18:12:50 +0100

    content improvement

--- content/basics/hello.java
+++ content/basics/hello.java
@@ -1,3 +1,5 @@
-public static void main(String[] args) {
-    System.out.println("Hello, World");
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
 }
