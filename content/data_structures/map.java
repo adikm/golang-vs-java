e2d5989f0bf4ae3c99454c9003999df546a7f91b
Bruno Borges <bruno.borges@gmail.com>
2020-02-05 13:41:44 -0800

    Bug fix on map.java
    1. Map.of returns an unmodifiable Map. You need a new Hashmap to put/remove
    2. missing semi-colon in line 5
    3. containsKey() is the correct method name

--- content/data_structures/map.java
+++ content/data_structures/map.java
@@ -1,10 +1,10 @@
-Map<String, String> mapA = Map.of("key1", "element1");
+var mapA = new HashMap(Map.of("key1", "element1"));
 
 mapA.put("key2", "element2");
 
-mapA.remove("key2")
+mapA.remove("key2");
 
-if (mapA.contains("key1")) {
+if (mapA.containsKey("key1")) {
     System.out.println(mapA.get("key1")); // element2
 }
 
